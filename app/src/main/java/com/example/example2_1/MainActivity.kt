10e60abaf8d4f1f163e564929c85e2adb7e569c1
emil-soderholm <soderholmemil@gmail.com>
2026-01-30 12:18:56 +0200

    lifecyclelog implemented

--- app/src/main/java/com/example/example2_1/MainActivity.kt
+++ app/src/main/java/com/example/example2_1/MainActivity.kt
@@ -1,14 +1,30 @@
 package com.example.example2_1
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var lastTime: Long = 0
+
+    private fun logElapsedTime(event: String) {
+        val currentTime = System.currentTimeMillis()
+        if (lastTime != 0L) {
+            val elapsed = currentTime - lastTime
+            Log.d("LifecycleLog", "$event: Elapsed time since last event: ${elapsed}ms")
+        } else {
+            Log.d("LifecycleLog", "$event: First event")
+        }
+        lastTime = currentTime
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        logElapsedTime("onCreate")
         setContentView(R.layout.activity_main)
 
         val editFirstName = findViewById<EditText>(R.id.edit_first_name)
@@ -33,5 +49,35 @@ class MainActivity : AppCompatActivity() {
             editPhone.text.clear()
             editComment.text.clear()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        logElapsedTime("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        logElapsedTime("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        logElapsedTime("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        logElapsedTime("onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        logElapsedTime("onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        logElapsedTime("onDestroy")
     }
 }
